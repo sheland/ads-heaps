75ba788f851bb1d99972e9ce63342fc963a23d67
Dan Roberts <dan@adadevelopersacademy.org>
2020-05-16 13:52:40 -0700

    Add testing for max heap internals, finish insert implementation

--- src/data_structures/heap.js
+++ src/data_structures/heap.js
@@ -30,7 +30,11 @@ class MaxHeap {
   }
 
   _parent(i) {
-    return i / 2;
+    return Math.floor(i / 2);
+  }
+
+  _maxHeapify(i) {
+
   }
 
   /**
@@ -44,7 +48,31 @@ class MaxHeap {
     if (this._count === this.size) {
       throw new Error('Heap is full!');
     }
+
     this._count += 1;
+    let i = this._count;
+    // console.log(`Inserted record with priority ${priority} at location ${i}`);
+
+    const record = this._storage[i];
+    record.priority = priority;
+    record.element = element;
+
+    let p = this._parent(i);
+    let parent = this._storage[p];
+    while (parent && record.priority > parent.priority) {
+      // console.log(`Swapping record at ${i} (pri ${record.priority}) with ${p} (pri ${parent.priority})`);
+      // Swap record and parent (bubble up)
+      // Note: in a language like C, Java or Rust, where the array is full of records
+      // instead of references to records, we would need to swap the priority and
+      // the reference to the element instead of the records themselves.
+      this._storage[p] = record;
+      this._storage[i] = parent;
+
+      // Reset i to the new location, take a reference to the new parent, and repeat
+      i = p;
+      p = this._parent(i);
+      parent = this._storage[p];
+    }
   }
 
   /**
@@ -53,7 +81,14 @@ class MaxHeap {
    * @returns {*} The data stored in the highest-priority record
    */
   removeNext() {
+    if (this._count === 0) {
+      return undefined;
+    }
 
+    const record = this._storage[1];
+
+    this._count -= 1;
+    return record.element;
   }
 
   /** 
--- src/data_structures/test/heap.test.js
+++ src/data_structures/test/heap.test.js
@@ -1,17 +1,36 @@
 import MaxHeap from '../heap';
 
+const isMaxHeap = function () {
+  // Special case for the root (no parent)
+  if (this._count > 0) {
+    const record = this._storage[1];
+    expect(record.priority).toBeDefined();
+  }
+
+  for (let i = 2; i <= this._count; i += 1) {
+    const record = this._storage[i];
+    const parent = this._storage[this._parent(i)];
+
+    expect(record.priority).toBeDefined();
+    expect(parent.priority).toBeGreaterThanOrEqual(record.priority);
+  }
+
+  return true;
+}
+
 describe(MaxHeap, () => {
   let heap;
 
   beforeEach(() => {
     heap = new MaxHeap();
+    heap.isMaxHeap = isMaxHeap;
   });
 
   it('starts empty', () => {
     expect(heap.count()).toBe(0);
   });
 
-  describe('insert', () => {
+  describe('insert interface', () => {
     it('increases count by one', () => {
       heap.insert(3, 'test');
       expect(heap.count()).toBe(1);
@@ -28,7 +47,7 @@ describe(MaxHeap, () => {
       }
       expect(heap.count()).toBe(MaxHeap.DEFAULT_SIZE);
     });
-    
+
     it('throws if the queue is full', () => {
       for (let i = 0; i < MaxHeap.DEFAULT_SIZE; i += 1) {
         heap.insert(i, `element_${i}`);
@@ -54,41 +73,85 @@ describe(MaxHeap, () => {
     });
   });
 
-  describe.skip('removeNext', () => {
-    it('removes the only element and reduces the count', () => {
-      
+  describe('insert implemntation', () => {
+    it('is a max heap after 1 insert', () => {
+      heap.insert(3, 'test');
+      expect(heap.isMaxHeap()).toBeTruthy();
+    });
+
+    it('is a max heap after sorted inserts', () => {
+      [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].forEach(i => {
+        heap.insert(i, `element_${i}`);
+        expect(heap.isMaxHeap()).toBeTruthy();
+      });
+    });
+
+    it('is a max heap after reverse-sorted inserts', () => {
+      [9, 8, 7, 6, 5, 4, 3, 2, 1].forEach(i => {
+        heap.insert(i, `element_${i}`);
+        expect(heap.isMaxHeap()).toBeTruthy();
+      });
+    });
+
+    it('is a max heap after random inserts', () => {
+      [3, 7, 0, 8, 2, 5, 1, 4, 9, 6].forEach(i => {
+        heap.insert(i, `element_${i}`);
+        expect(heap.isMaxHeap()).toBeTruthy();
+      });
+    });
+  });
+
+  describe('removeNext', () => {
+    it('removes the only record, returns its element, and reduces the count', () => {
+      heap.insert(3, 'test');
+
+      expect(heap.removeNext()).toBe('test');
+      expect(heap.count()).toBe(0);
+    });
+
+    it('returns undefined and does not reduce the count if the heap is empty', () => {
+      expect(heap.removeNext()).toBeUndefined();
+      expect(heap.count()).toBe(0);
+
+      const elements = ['a', 'b', 'c'];
+      elements.forEach((word, i) => heap.insert(i, word));
+      elements.forEach(() => heap.removeNext());
+      expect(heap.count()).toBe(0);
+
+      expect(heap.removeNext()).toBeUndefined();
+      expect(heap.count()).toBe(0);
     });
 
     it('removes the max element if it was inserted first', () => {
-      
+
     });
 
     it('removes the max element if it was inserted last', () => {
-      
+
     });
 
     it('removes the max element if it was inserted in the middle', () => {
-      
+
     });
 
     it('removes all sorted input in priority order', () => {
-      
+
     });
 
     it('removes all reverse-sorted input in priority order', () => {
-      
+
     });
 
     it('removes all random input in priority order', () => {
-      
+
     });
 
     it('prevents the heap from overflowing', () => {
-      
+
     });
   });
 
   describe('count', () => {
-    
+
   });
 });

9a27e7ef89fd52b2c2a147811762f7afaf3d0cf5
Dan Roberts <dan@adadevelopersacademy.org>
2020-05-16 12:09:08 -0700

    Add interfaces

---
+++ src/interfaces/priority_queue.js
@@ -0,0 +1,38 @@
+class PriorityQueue {
+  /**
+   * Create a new empty priority queue of a given size
+   * 
+   * @param {number} size Maximum capacity of the queue
+   */
+  constructor(size = 1024) {
+
+  }
+
+  /**
+   * Add a record to the queue with a given priority
+   * 
+   * @param {number} priority Priority of the record
+   * @param {*} element Data to store in this record
+   */
+  insert(priority, element) {
+
+  }
+
+  /**
+   * Remove and return the record with the highest priority
+   * 
+   * @returns {*} The data stored in the highest-priority record
+   */
+  next() {
+
+  }
+
+  /** 
+   * How many records are in the priority queue?
+   * 
+   * @returns {number} Record count
+   */
+  count() {
+
+  }
+}
